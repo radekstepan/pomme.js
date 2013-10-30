501367c0cc1f2a3ed051a181273e34dc022e3b02
Radek Stepan <radekstepan@users.noreply.github.com>
2013-10-30 23:31:16 +0000

    liable to be overriden

--- tasks/moulds/wrapper.eco.js
+++ tasks/moulds/wrapper.eco.js
@@ -1,11 +1,9 @@
 <% clean = (input) => %><%- input.replace(/\n\s*\n/g, '\n') %><% end %>
 (function() {
-    var root = this;
-
     <%- clean @content %>
 
     // Use our or outside require?
-    root.require = (root.require) ? root.require : require;
+    this.require = (this.require) ? this.require : require;
 
     // Expose the app.
     require.alias("<%- @package %>/<%- @main %>.js", "<%- @package %>/index.js");
